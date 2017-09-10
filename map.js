32c666c76d0e830cda80e802d75d08d6a0edf5b4
Parina Kay <time-is-immortal@users.noreply.github.com>
2017-09-10 07:34:09 -0400

    Update map.js
    Philly area

--- map.js
+++ map.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Platform} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Dimensions} from 'react-native';
 import { Constants, Location, Permissions } from 'expo';
-import { MapView } from 'expo';
-export default class App extends React.Component {
-  static navigationOptions = {
-    title: 'map',
-    }
+import MapView from 'react-native-maps'; //0.16.3
+import "prop-types"; //15.5.10
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 39.9515;
+const LONGITUDE = -75.1911;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
+export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  onPositionUpdate(newlocation){
+   console.log(newlocation);
+  }
+
   componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
       this.setState({
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
       });
     } else {
-      this._getLocationAsync();
+      Location.watchPositionAsync({enableHighAccuracy:true}, this.onPositionUpdate);
     }
   }
+
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
@@ -22,20 +40,48 @@ export default class App extends React.Component {
         errorMessage: 'Permission to access location was denied',
       });
     }
+
     let location = await Location.getCurrentPositionAsync({});
     this.setState({ location });
   };
+
   render() {
+    let text = 'Waiting...';
+      if(this.state.errorMessage){
+        text = this.state.errorMessage;
+      }
+      else if (this.state.location){
+        text = JSON.stringify(this.state.location);
+      }
     return (
-      <MapView
+      /*
+     <View style={styles.container}>
+        <Text style={styles.paragraph}>{text}</Text>
+     </View>
+     */
+     <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }} /> 
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
